b71221b06b2e5ebaf505bf907044d2ed0b7768a5
kingyj00 <onshopyj@gmail.com>
2025-01-07 17:41:02 +0900

    Page2-08

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/main")
+    @GetMapping("/one")
     @ResponseBody
     public String index() {
-        return ("Main Page");
+        return "What up SpringBoots? I'm gonna beat u down.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
